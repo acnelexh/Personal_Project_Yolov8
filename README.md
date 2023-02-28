2be7f9eb3d4a4594d862b5ca7a5761db6dd3a0f9
chenzyu <chenzyu@umich.edu>
2023-02-28 17:56:43 +0000

    added yolo training script

--- README.md
+++ README.md
@@ -4,9 +4,15 @@ It consist of two parts
 
 (1) Yolo v8
 
+For the Yolo v8 training and evaluation, I am using the ultranalytics code base.
+
 (2) Faster-RCNN
+
+For the Faster-RCNN training and evalution, I am using the mmdetection code base.
+
 
 Dataset
 
 I used fiftyone to convert the Kitti dataset to COCO format, and kitti_for_yolo to convert Kitti dataset to YOLO format.
+
 
---
+++ yolo/train_yolo.py
@@ -0,0 +1,24 @@
+from ultralytics import YOLO
+from pathlib import Path
+import os
+
+# Train YOLOv8 on kitti for 10 epochs
+
+batch_size = 16
+epochs = 10
+learning_rate = 1E-3
+img_size = 640
+data_path = os.path.normpath('/home/chenzyu/datasets/kitti_yolo')
+weight_path = os.path.normpath('/home/chenzyu/Personal_Project_Yolov8/yolo/yolov8n.pt')
+
+model = YOLO(weight_path)
+
+model.train(
+    data=os.path.join(data_path, 'kitti.yaml'),
+    epochs=epochs,
+    batch=batch_size,
+    imgsz=img_size,
+    save=True,
+    pretrained=True,
+    project='kitti',
+    lr0=learning_rate)
